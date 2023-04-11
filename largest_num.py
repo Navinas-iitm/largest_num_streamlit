8b33a4236d8a4ad3a2c0aae25dbfcc40c1015ef4
Navinas-iitm <130460030+Navinas-iitm@users.noreply.github.com>
2023-04-11 16:40:39 +0530

    Update largest_num.py

--- largest_num.py
+++ largest_num.py
@@ -1,14 +1,9 @@
 import streamlit as st
 
-st.title("Largest Number")
+st.title("Largest Number by 21f2000930")
 
 num1 = st.number_input("Enter first number")
 num2 = st.number_input("Enter second number")
 num3 = st.number_input("Enter third number")
 
-if num1 > num2 and num1 > num3:
-    st.write("The largest number is", num1)
-elif num2 > num1 and num2 > num3:
-    st.write("The largest number is", num2)
-else:
-    st.write("The largest number is", num3)
+st.write("The largest number is", max(num1, num2, num3))
