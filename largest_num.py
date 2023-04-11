ab827f743bccb0db081dd5cd2e9c5aafc775dced
Navinas-iitm <130460030+Navinas-iitm@users.noreply.github.com>
2023-04-11 16:36:59 +0530

    Update largest_num.py

--- largest_num.py
+++ largest_num.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.title("Largest Number")
+
+num1 = st.number_input("Enter first number")
+num2 = st.number_input("Enter second number")
+num3 = st.number_input("Enter third number")
+
+if num1 > num2 and num1 > num3:
+    st.write("The largest number is", num1)
+elif num2 > num1 and num2 > num3:
+    st.write("The largest number is", num2)
+else:
+    st.write("The largest number is", num3)
